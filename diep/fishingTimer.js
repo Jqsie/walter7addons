492ea32b430b1a81b6e96f019c4a0039458bfcb9
Jqsie <166062648+Jqsie@users.noreply.github.com>
2024-11-13 03:18:36 +0800

    initial

---
+++ diep/fishingTimer.js
@@ -0,0 +1,62 @@
+import Settings from '../config';
+
+
+const EntityArmorStandClass = Java.type(
+  'net.minecraft.entity.item.EntityArmorStand'
+).class;
+const EntityFishHookClass = Java.type(
+  'net.minecraft.entity.projectile.EntityFishHook'
+).class;
+
+
+const fishingTimerRegex = /^\d\.\d$/;
+
+const display = new Display();
+display.setOrder("down");
+display.setRenderLoc((Renderer.screen.getWidth() - display.getWidth())/ 2.0, Renderer.screen.getHeight() / 2.0 - 15);
+
+var textSize = (Settings.fishingTimerSize + 1);
+
+// register('command', (diep) => {
+//   if (diep) {
+//     textSize = diep;
+//   } else {
+//     textSize = 1.0;
+//   }
+//  
+// }).setName('diepsize');
+
+
+register('renderWorld', () => {
+  if (Settings.fishingTimer) {
+    let found = false;
+    var textSize = (Settings.fishingTimerSize + 1);
+    World.getAllEntitiesOfType(EntityArmorStandClass).forEach((armorStands) => {
+      const name = armorStands.getName().removeFormatting();
+      if (name.match(fishingTimerRegex)) {
+        armorStands.setPosition(0, -100, 0);
+        display.setTextColor(Renderer.YELLOW)
+        let line = new DisplayLine(name);
+        line.setShadow(true);
+        line.setScale(textSize);
+        display.setLine(0, line);
+        display.setRenderLoc((Renderer.screen.getWidth() - display.getWidth())/ 2.0, Renderer.screen.getHeight() / 2.0 - 15);
+        found = true;
+      } else if (name === '!!!') {
+        let line = new DisplayLine("!!!");
+    armorStands.setPosition(0, -100, 0);
+        display.setTextColor(Renderer.RED)
+        line.setShadow(true);
+        line.setScale(textSize);
+        display.setLine(0, line);
+        display.setRenderLoc((Renderer.screen.getWidth() - display.getWidth())/ 2.0, Renderer.screen.getHeight() / 2.0 - 15);
+        found = true;
+      } else {
+        display.setLine(0, '');
+      }
+    });
+}});
+
+export const openFishingTimer = () => {
+  a = 5;
+};
---
+++ diep/musicDisc.js
@@ -0,0 +1,21 @@
+import Settings from "../config";
+import { registerWhen } from "../whoiy/helperFunctions";
+
+const records = ["records.11", "records.13", "records.blocks", "records.cat", 
+    "records.chirp", "records.far", "records.mall", "records.mellohi", "records.stal",
+    "records.strad", "records.wait", "records.ward"
+  ]
+
+register("worldLoad", () => {
+    if (Settings.musicDisc) {
+        // console.log('hello!')
+        new Thread(() => {
+            Thread.sleep(100);
+            World.playRecord(records[Math.round(Math.random() * records.length)], Player.getX(), Player.getY(), Player.getZ());
+        }).start()
+    }
+  })
+
+function stopMusicDisc() {
+    World.stopAllSounds()
+}
