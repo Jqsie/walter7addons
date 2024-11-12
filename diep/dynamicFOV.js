d3e94d70d176c9bf663c6b122335886de5221cf4
Jqsie <jqsie@proton.me>
2024-11-13 04:14:38 +0800

    added dynamic fov

---
+++ diep/dynamicFOV.js
@@ -0,0 +1,19 @@
+import Settings from '../config';
+
+var t = 0.0;
+var movingFOV = false;
+
+register("step", (diep) => {
+    if (Settings.dynamicFOV) {
+        t += 1.0/60.0;
+        Client.settings.setFOV(Math.abs(Math.sin(t) * 110.0) + 1.0);
+        movingFOV = true;
+    }
+  }).setFps(60);
+
+register("step", (diep) => {
+    if (movingFOV && !Settings.dynamicFOV) {
+        Client.settings.setFOV(Settings.previousFOV)
+        var movingFOV = false
+    }
+})
