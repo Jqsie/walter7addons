e44d3949addd920c8f3e6228fd11c1dceeb797c5
watermelob <watermelob@proton.me>
2024-11-13 00:30:21 -0500

    fix

--- diep/luckyBlockFix.js
+++ diep/luckyBlockFix.js
@@ -1,75 +1,49 @@
 import Settings from '../config';
 
+var inLuckyGame = false;
+
 register("spawnParticle", (particle, type, event) => {
-  
+  if (Settings.luckyBlockFix) {
     if(type.toString() != "SPELL_WITCH" && type.toString() != "NOTE") {
       return;
     }
-  
-    var luckyBlock = false;
-    var lines = Scoreboard.getLines();
-    if (Settings.luckyBlockFix) {
-        for (let i = 0; i < lines.length; i++) {
-            if (lines[i]) {
-              if ((ChatLib.removeFormatting(lines[i].toString()).search("Lab: Luc") != -1)) {
-                luckyBlock = true;
-                break
-        
-              }
-            }
-        }
-    }
-    if (luckyBlock) {
+
+    if (inLuckyGame) {
         cancel(event);
     }
-  });
-  
-  
-  register("soundPlay", (pos, name, vol, pitch, cat, event) => {
-  
-    if (cat) {
-      if (cat.toString() != "RECORDS") {
-        return
-      }
-    }
-  
-    var luckyBlock = false;
-    var lines = Scoreboard.getLines();
-    if (Settings.luckyBlockFix) {
-        for (let i = 0; i < lines.length; i++) {
-            if (lines[i]) {
-                if ((ChatLib.removeFormatting(lines[i].toString()).search("Lab: Luc") != -1)) {
-                luckyBlock = true;
-                break
-                }
-            }
-        }
-    }
-  
-    if (cat) {
-      if (luckyBlock) {
-        if (Math.abs(pos.x - Player.getX()) <= 1.0 && Math.abs(pos.y - Player.getY()) <= 1.0 && Math.abs(pos.z - Player.getZ()) <= 1.0) {
-          print(`Pos: ${pos.toString()} Name: ${name.toString()} Vol: ${vol.toString()} Pitch: ${pitch.toString()}`);
-          cancel(event);
-        }
-        
-      }
-    }
-     
-    
+  }
   });
   
   register("renderTitle", (title, subtitle, event) => {
     if (Settings.luckyBlockFix) {
-    var x = ChatLib.removeFormatting(title);
-    if (x == "SkyWars Laboratory" || x == "SkyWars" || x == "Lucky Rule" || x == "Ninja Mode") {
-      cancel(event);
-    } else {
-      if (!titleList.includes(x)) {
-        print(x);
-        titleList.push(x);
+      let x = ChatLib.removeFormatting(title);
+      if (x == "SkyWars Laboratory" || x == "Lucky Rule" || x == "Ninja mode!") {
+        cancel(event);
       }
-      
-    }
     }
   });
+
+  register("worldLoad", () => {
+    inLuckyGame = false;
+  });
+
+  register("chat", () => {
+    if (findInScoreboard("Lab: Luc")) {
+      inLuckyGame = true;
+      ChatLib.chat(inLuckyGame);
+    }
+  }).setCriteria("Cages opened! FIGHT!")
+
+  function findInScoreboard(str) {
+    let found = false;
+    let lines = Scoreboard.getLines();
+    for (let i = 0; i < lines.length; i++) {
+      if (lines[i]) {
+        if ((ChatLib.removeFormatting(lines[i].toString()).search(str) != -1)) {
+          found = true;
+          return found;
+        }
+      }
+    }
+    return found;
+  }
