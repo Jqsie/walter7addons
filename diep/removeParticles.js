0624c96346aaac933e09a14c25429b7a9aceb954
Jqsie <jqsie@proton.me>
2024-11-14 14:27:30 +0800

    fix remove particle

--- diep/removeParticles.js
+++ diep/removeParticles.js
@@ -17,21 +17,7 @@ register("spawnParticle", (particle, type, event) => {
 register("chat", () => {
     title = Scoreboard.getTitle();
     title = title.replace(/[a-z0-9§]/g, "");
-    if (title == "BED WARS" && findInScoreboard("Red:")) {
+    if (title == "BED WARS") {
       inBedwarsGame = true;
     }
   }).setCriteria("Protect your bed and destroy the enemy beds.").setContains();
-
-function findInScoreboard(str) {
-    let found = false;
-    let lines = Scoreboard.getLines();
-    for (let i = 0; i < lines.length; i++) {
-      if (lines[i]) {
-        if ((ChatLib.removeFormatting(lines[i].toString()).search(str) != -1)) {
-          found = true;
-          return found;
-        }
-      }
-    }
-    return found;
-  }
