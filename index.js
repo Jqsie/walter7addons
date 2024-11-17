9cda13efc5ab2f45a0824a95f837791ea5d08358
watermelob <watermelob@proton.me>
2024-11-17 03:03:38 -0500

    added invisible shop feature (needs improved)

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import './features/miningFatigue'
 import './features/slumberHotel'
 import './features/removeParticles'
 import './features/emeraldSpawn'
+import './features/invisibleShop'
 //import './features/blockHighlight'
 
 register("command", () => Settings.openGUI()).setName("walter");
